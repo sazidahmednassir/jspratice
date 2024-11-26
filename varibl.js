69fb63bba75b727028763807752f6c7a404cf9fc
sazidahmednassir <sazidahmednassir@gmail.com>
2024-11-27 00:22:54 +0600

    variable  pratice

---
+++ varibl.js
@@ -0,0 +1,14 @@
+var x=20, y=30;
+
+console.log(x, y);
+
+var a="sazid", b=30;
+console.log(a, b);
+
+let m="nassir", n=20;
+console.log(m, n);
+
+console.log(typeof(m));
+
+const t="ahmed";
+console.log(t);
